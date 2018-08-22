88b02b3067e090d70d32f2164bcd4cd68ea28bad
Isaiah Harris <harrisfambam@gmail.com>
2018-08-21 14:37:43 -1000

    routing works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router'
+//pages
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+
+//shared components
 import { HeaderComponent } from './components/header/header.components';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      [
+        { path: '', component: HomeComponent },
+        { path: 'login', component: LoginComponent }
+      ]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
